93a40ac0480cd19d29754fdfe824fe53ee8a45e9
youdehai <haivo@foxmail.com>
2023-09-28 10:40:37 +0800

    feat: kotlinx serialization

--- app/src/main/java/com/github/joehaivo/httpcall/BannerVo.kt
+++ app/src/main/java/com/github/joehaivo/httpcall/BannerVo.kt
@@ -2,15 +2,17 @@ package com.github.joehaivo.httpcall
 
 
 import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
 
 @Keep
+@Serializable
 data class BannerVo(
     val desc: String? = "",
-    val id: Int? = null,
+    val id: Int? = 666,
     val imagePath: String? = "",
     val isVisible: Int? = null,
     val order: Int? = null,
-    val title: String? = "",
-    val type: Int? = null,
+    val title: String? = "sss",
+    val type: Int? = 5,
     val url: String? = ""
 )
--- app/src/main/java/com/github/joehaivo/httpcall/MainActivity.kt
+++ app/src/main/java/com/github/joehaivo/httpcall/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.lifecycleScope
+import com.blankj.utilcode.util.GsonUtils
 import com.blankj.utilcode.util.ToastUtils
 import com.github.joehaivo.httpcall.databinding.ActivityMainBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotlin.coroutines.resume
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val json1 = Json { encodeDefaults = true }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +45,7 @@ class MainActivity : AppCompatActivity() {
                 if (response1.isOk() && response2.isOk()) {
                     binding.tvContent.text = "${response1.data} #\n\n${response2.data}"
                 } else {
-                    binding.tvContent.text ="${response1.exception} #\n\n${response2.exception}"
+                    binding.tvContent.text = "${response1.exception} #\n\n${response2.exception}"
                 }
             }
         }
@@ -65,6 +71,35 @@ class MainActivity : AppCompatActivity() {
             Log.d("TAG", "onCreate: serialno     =$serialno")
         }
 
+        binding.btnGson.setOnClickListener {
+            var bannerVo: BannerVo
+            var bannerJson: String
+            val millis = measureTimeMillis {
+                repeat(100000) { i ->
+                    bannerJson = GsonUtils.toJson(BannerVo(desc = "desc$i"))
+                    bannerVo = GsonUtils.fromJson(bannerJson, BannerVo::class.java)
+                    if (i % 10000 == 0) {
+                        Log.d("TAG", "gson encode:$bannerJson, decode:$bannerVo")
+                    }
+                }
+            }
+            ToastUtils.showLong("Gson spend ${millis}ms")
+        }
+        binding.btnSerial.setOnClickListener {
+            val json2 = Json { encodeDefaults = true }
+            val millis = measureTimeMillis {
+                var bannerVo: BannerVo
+                var bannerJson: String
+                repeat(100000) { i ->
+                    bannerJson = json2.encodeToString(BannerVo(desc = "desc$i"))
+                    bannerVo = json2.decodeFromString(bannerJson)
+                    if (i % 10000 == 0) {
+                        Log.d("TAG", "ktJson encode:$bannerJson, decode:$bannerVo")
+                    }
+                }
+            }
+            ToastUtils.showLong("ktJson spend ${millis}ms")
+        }
     }
 
     suspend fun showDialog(title: String, content: String) = suspendCancellableCoroutine { continuation ->
